2d7997da6c35ddcf8c118cdb19530df13a96d4e1
Steffan Andrews <stef@orchetect.com>
2024-10-26 00:28:34 -0700

    Unit test cleanup

--- Tests/PrefsKitTests/PrefsKitTests.swift
+++ Tests/PrefsKitTests/PrefsKitTests.swift
@@ -3,41 +3,44 @@ import Testing
 
 // MARK: Mock Types
 
-enum RawEnum: String, RawRepresentable {
-    case one
-}
-
-// MARK: - Protocol Adoptions
-struct Foo: PrefKey {
-    typealias StorageValue = Bool
-    let key: String = ""
-}
-
-struct Bar: DefaultedPrefKey {
-    typealias StorageValue = Bool
-    let key: String = ""
-    let defaultValue: StorageValue = true
-}
-
-struct RawFoo: RawRepresentablePrefKey {
-    typealias T = RawEnum
-    let key: String = ""
-}
-
-struct RawBar: RawRepresentableDefaultedPrefKey {
-    typealias T = RawEnum
-    let key: String = ""
-    let defaultValue: T = .one
-}
-
-let foo = Foo()
-let bar = Bar()
-let rawFoo = RawFoo()
-let rawbar = RawBar()
-
-@Test func basic() async throws {
-    _ = foo.getValue()
-    _ = bar.getValueDefaulted()
-    _ = rawFoo.getValue()
-    _ = rawbar.getValueDefaulted()
+@Suite("Check if basic syntax compiles")
+struct BasicSyntax {
+    enum RawEnum: String, RawRepresentable {
+        case one
+    }
+    
+    // MARK: - Protocol Adoptions
+    struct Foo: PrefKey {
+        typealias StorageValue = Bool
+        let key: String = ""
+    }
+    
+    struct Bar: DefaultedPrefKey {
+        typealias StorageValue = Bool
+        let key: String = ""
+        let defaultValue: StorageValue = true
+    }
+    
+    struct RawFoo: RawRepresentablePrefKey {
+        typealias T = RawEnum
+        let key: String = ""
+    }
+    
+    struct RawBar: RawRepresentableDefaultedPrefKey {
+        typealias T = RawEnum
+        let key: String = ""
+        let defaultValue: T = .one
+    }
+    
+    let foo = Foo()
+    let bar = Bar()
+    let rawFoo = RawFoo()
+    let rawbar = RawBar()
+    
+    @Test func basicGetValueCalls() async throws {
+        _ = foo.getValue()
+        _ = bar.getValueDefaulted()
+        _ = rawFoo.getValue()
+        _ = rawbar.getValueDefaulted()
+    }
 }
