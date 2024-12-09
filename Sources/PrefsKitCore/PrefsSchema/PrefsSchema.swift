f1d1a163ae1c000fbb1a03a4ed4051b7d5226015
Steffan Andrews <stef@orchetect.com>
2024-12-09 15:05:12 -0800

    `PrefsSchema`: Removed associated `Key` type requirement, replaced with String

--- Sources/PrefsKitCore/PrefsSchema/PrefsSchema.swift
+++ Sources/PrefsKitCore/PrefsSchema/PrefsSchema.swift
@@ -9,8 +9,6 @@ import Foundation
 /// Protocol for prefs schema.
 @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
 public protocol PrefsSchema where Self: Sendable {
-    associatedtype Key: RawRepresentable where Key: Hashable, Key: CaseIterable, Key: Sendable, Key.RawValue == String
-    
     var storage: any PrefsStorage { get }
 }
 
@@ -33,83 +31,83 @@ extension PrefsSchema {
     
     /// Synthesize a pref key with an `Int` value.
     public func pref(
-        int key: Key
+        int key: String
     ) -> ObservablePref<IntPrefKey> {
-        let keyInstance = IntPrefKey(key: key.rawValue)
+        let keyInstance = IntPrefKey(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with an `String` value.
     public func pref(
-        string key: Key
+        string key: String
     ) -> ObservablePref<StringPrefKey> {
-        let keyInstance = StringPrefKey(key: key.rawValue)
+        let keyInstance = StringPrefKey(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Bool` value.
     public func pref(
-        bool key: Key
+        bool key: String
     ) -> ObservablePref<BoolPrefKey> {
-        let keyInstance = BoolPrefKey(key: key.rawValue)
+        let keyInstance = BoolPrefKey(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Double` value.
     public func pref(
-        double key: Key
+        double key: String
     ) -> ObservablePref<DoublePrefKey> {
-        let keyInstance = DoublePrefKey(key: key.rawValue)
+        let keyInstance = DoublePrefKey(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Float` value.
     public func pref(
-        float key: Key
+        float key: String
     ) -> ObservablePref<FloatPrefKey> {
-        let keyInstance = FloatPrefKey(key: key.rawValue)
+        let keyInstance = FloatPrefKey(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Data` value.
     public func pref(
-        data key: Key
+        data key: String
     ) -> ObservablePref<DataPrefKey> {
-        let keyInstance = DataPrefKey(key: key.rawValue)
+        let keyInstance = DataPrefKey(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with an `Array` value.
     public func pref(
-        array key: Key
+        array key: String
     ) -> ObservablePref<AnyArrayPrefKey> {
-        let keyInstance = AnyArrayPrefKey(key: key.rawValue)
+        let keyInstance = AnyArrayPrefKey(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with an `Array` value.
     public func pref<Element: PrefStorageValue>(
-        array key: Key,
+        array key: String,
         of elementType: Element.Type
     ) -> ObservablePref<ArrayPrefKey<Element>> {
-        let keyInstance = ArrayPrefKey<Element>(key: key.rawValue)
+        let keyInstance = ArrayPrefKey<Element>(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Dictionary` value.
     public func pref<Element: PrefStorageValue>(
-        dictionary key: Key,
+        dictionary key: String,
         of elementType: Element.Type
     ) -> ObservablePref<DictionaryPrefKey<Element>> {
-        let keyInstance = DictionaryPrefKey<Element>(key: key.rawValue)
+        let keyInstance = DictionaryPrefKey<Element>(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Dictionary` value.
     public func pref(
-        dictionary key: Key
+        dictionary key: String
     ) -> ObservablePref<AnyDictionaryPrefKey> {
-        let keyInstance = AnyDictionaryPrefKey(key: key.rawValue)
+        let keyInstance = AnyDictionaryPrefKey(key: key)
         return pref(keyInstance)
     }
     
@@ -117,93 +115,93 @@ extension PrefsSchema {
     
     /// Synthesize a pref key with an `Int` value with a default value.
     public func pref(
-        int key: Key,
+        int key: String,
         default defaultValue: Int
     ) -> ObservableDefaultedPref<DefaultedIntPrefKey> {
-        let keyInstance = DefaultedIntPrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedIntPrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `String` value with a default value.
     public func pref(
-        string key: Key,
+        string key: String,
         default defaultValue: String
     ) -> ObservableDefaultedPref<DefaultedStringPrefKey> {
-        let keyInstance = DefaultedStringPrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedStringPrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Bool` value with a default value.
     public func pref(
-        bool key: Key,
+        bool key: String,
         default defaultValue: Bool
     ) -> ObservableDefaultedPref<DefaultedBoolPrefKey> {
-        let keyInstance = DefaultedBoolPrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedBoolPrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Double` value with a default value.
     public func pref(
-        double key: Key,
+        double key: String,
         default defaultValue: Double
     ) -> ObservableDefaultedPref<DefaultedDoublePrefKey> {
-        let keyInstance = DefaultedDoublePrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedDoublePrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Float` value with a default value.
     public func pref(
-        float key: Key,
+        float key: String,
         default defaultValue: Float
     ) -> ObservableDefaultedPref<DefaultedFloatPrefKey> {
-        let keyInstance = DefaultedFloatPrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedFloatPrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with a `Data` value with a default value.
     public func pref(
-        data key: Key,
+        data key: String,
         default defaultValue: Data
     ) -> ObservableDefaultedPref<DefaultedDataPrefKey> {
-        let keyInstance = DefaultedDataPrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedDataPrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with an `Array` value with a default value.
     public func pref(
-        array key: Key,
+        array key: String,
         default defaultValue: AnyPrefArray
     ) -> ObservableDefaultedPref<DefaultedAnyArrayPrefKey> {
-        let keyInstance = DefaultedAnyArrayPrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedAnyArrayPrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with an `Array` value with a default value.
     public func pref<Element: PrefStorageValue>(
-        array key: Key,
+        array key: String,
         of elementType: Element.Type,
         default defaultValue: [Element]
     ) -> ObservableDefaultedPref<DefaultedArrayPrefKey<Element>> {
-        let keyInstance = DefaultedArrayPrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedArrayPrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with an `Dictionary` value with a default value.
     public func pref(
-        dictionary key: Key,
+        dictionary key: String,
         default defaultValue: AnyPrefDictionary
     ) -> ObservableDefaultedPref<DefaultedAnyDictionaryPrefKey> {
-        let keyInstance = DefaultedAnyDictionaryPrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedAnyDictionaryPrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with an `Dictionary` value with a default value.
     public func pref<Element: PrefStorageValue>(
-        dictionary key: Key,
+        dictionary key: String,
         of elementType: Element.Type,
         default defaultValue: [String: Element]
     ) -> ObservableDefaultedPref<DefaultedDictionaryPrefKey<Element>> {
-        let keyInstance = DefaultedDictionaryPrefKey(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedDictionaryPrefKey(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
     
@@ -211,20 +209,20 @@ extension PrefsSchema {
     
     /// Synthesize a pref key with an `RawRepresentable` value.
     public func pref<Value: RawRepresentable, StorageValue: PrefStorageValue>(
-        _ key: Key,
+        _ key: String,
         of elementType: Value.Type
     ) -> ObservablePref<AnyRawRepresentablePrefKey<Value, StorageValue>> {
-        let keyInstance = AnyRawRepresentablePrefKey<Value, StorageValue>(key: key.rawValue)
+        let keyInstance = AnyRawRepresentablePrefKey<Value, StorageValue>(key: key)
         return pref(keyInstance)
     }
     
     /// Synthesize a pref key with an `RawRepresentable` value with a default value.
     public func pref<Value: RawRepresentable, StorageValue: PrefStorageValue>(
-        _ key: Key,
+        _ key: String,
         of elementType: Value.Type,
         default defaultValue: Value
     ) -> ObservableDefaultedPref<DefaultedAnyRawRepresentablePrefKey<Value, StorageValue>> {
-        let keyInstance = DefaultedAnyRawRepresentablePrefKey<Value, StorageValue>(key: key.rawValue, defaultValue: defaultValue)
+        let keyInstance = DefaultedAnyRawRepresentablePrefKey<Value, StorageValue>(key: key, defaultValue: defaultValue)
         return pref(keyInstance)
     }
 }
