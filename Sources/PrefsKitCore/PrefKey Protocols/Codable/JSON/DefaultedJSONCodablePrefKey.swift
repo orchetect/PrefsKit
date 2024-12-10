e3631b8832bc9b06b23f275f6a8a5bb070fc4c26
Steffan Andrews <stef@orchetect.com>
2024-12-09 18:37:51 -0800

    Inline docs

--- Sources/PrefsKitCore/PrefKey Protocols/Codable/JSON/DefaultedJSONCodablePrefKey.swift
+++ Sources/PrefsKitCore/PrefKey Protocols/Codable/JSON/DefaultedJSONCodablePrefKey.swift
@@ -6,6 +6,10 @@
 
 import Foundation
 
-/// A prefs key that encodes and decodes a `Codable` type to/from raw storage using JSON encoding
-/// and provides a default value.
+/// A prefs key that encodes and decodes a `Codable` type to/from raw storage using JSON encoding with default
+/// options and provides a default value.
+///
+/// > Note:
+/// > If custom `JSONEncoder`/`JSONDecoder` options are required, override the default implementation(s) of
+/// > `prefEncoder()` and/or `prefDecoder()` methods to return an encoder/decoder with necessary options configured.
 public protocol DefaultedJSONCodablePrefKey: JSONCodablePrefKey, DefaultedCodablePrefKey { }
--- Sources/PrefsKitCore/PrefKey Protocols/Codable/JSON/JSONCodablePrefKey.swift
+++ Sources/PrefsKitCore/PrefKey Protocols/Codable/JSON/JSONCodablePrefKey.swift
@@ -7,16 +7,21 @@
 import Combine
 import Foundation
 
-/// A prefs key that encodes and decodes a `Codable` type to/from raw `Data` storage using JSON encoding.
+/// A prefs key that encodes and decodes a `Codable` type to/from raw `Data` storage using JSON encoding with default
+/// options.
+///
+/// > Note:
+/// > If custom `JSONEncoder`/`JSONDecoder` options are required, override the default implementation(s) of
+/// > `prefEncoder()` and/or `prefDecoder()` methods to return an encoder/decoder with necessary options configured.
 public protocol JSONCodablePrefKey: CodablePrefKey
 where Encoder == JSONEncoder, Decoder == JSONDecoder, StorageValue == Data { }
 
 extension JSONCodablePrefKey {
-    public func prefEncoder() -> Encoder {
+    public func prefEncoder() -> JSONEncoder {
         JSONEncoder()
     }
 
-    public func prefDecoder() -> Decoder {
+    public func prefDecoder() -> JSONDecoder {
         JSONDecoder()
     }
 }
