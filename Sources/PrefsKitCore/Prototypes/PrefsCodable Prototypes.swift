9f22e023c997dd2b8e9c1812192941e63f1afefd
Steffan Andrews <stef@orchetect.com>
2024-12-12 17:39:21 -0800

    Added `PrefsCodable` Prototypes

---
+++ Sources/PrefsKitCore/Prototypes/PrefsCodable Prototypes.swift
@@ -0,0 +1,70 @@
+//
+//  PrefsCodable Prototypes.swift
+//  PrefsKit • https://github.com/orchetect/PrefsKit
+//  © 2024 Steffan Andrews • Licensed under MIT License
+//
+
+import Combine
+import Foundation
+
+public struct AtomicPrefsCoding<Value>: AtomicPrefsCodable where Value: PrefsStorageValue {
+    public typealias Value = Value
+}
+
+public struct PrefsCoding<Value>: PrefsCodable where Value: PrefsStorageValue {
+    public typealias Value = Value
+    let encodeBlock: @Sendable (Value) -> StorageValue?
+    let decodeBlock: @Sendable (StorageValue) -> Value?
+    
+    init(
+        encodeBlock: @escaping @Sendable (Value) -> StorageValue?,
+        decodeBlock: @escaping @Sendable (StorageValue) -> Value?
+    ) {
+        self.encodeBlock = encodeBlock
+        self.decodeBlock = decodeBlock
+    }
+    
+    public func decode(prefsValue: StorageValue) -> Value? {
+        decodeBlock(prefsValue)
+    }
+    
+    public func encode(prefsValue: Value) -> StorageValue? {
+        encodeBlock(prefsValue)
+    }
+}
+
+public struct RawRepresentablePrefsCoding<Value>: RawRepresentablePrefsCodable
+where Value: Sendable, Value: RawRepresentable, Value.RawValue: PrefsStorageValue {
+    public typealias Value = Value
+    public typealias StorageValue = Value.RawValue
+}
+
+public struct CodablePrefsCoding<Value, StorageValue, Encoder, Decoder>: CodablePrefsCodable
+where Value: Codable, Value: Sendable,
+      StorageValue: PrefsStorageValue, StorageValue == Encoder.Output,
+      Encoder: TopLevelEncoder, Encoder: Sendable, Encoder.Output: PrefsStorageValue,
+      Decoder: TopLevelDecoder, Decoder: Sendable, Decoder.Input: PrefsStorageValue,
+      Encoder.Output == Decoder.Input
+{
+    public typealias Value = Value
+    public typealias StorageValue = StorageValue
+    public typealias Encoder = Encoder
+    public typealias Decoder = Decoder
+    let encoder: Encoder
+    let decoder: Decoder
+    
+    public init(
+        encoder: @escaping @Sendable @autoclosure () -> Encoder,
+        decoder: @escaping @Sendable @autoclosure () -> Decoder
+    ) {
+        self.encoder = encoder()
+        self.decoder = decoder()
+    }
+    
+    public func prefsEncoder() -> Encoder { encoder }
+    public func prefsDecoder() -> Decoder { decoder }
+}
+
+public struct JSONCodablePrefsCoding<Value>: JSONCodablePrefsCodable where Value: Codable, Value: Sendable {
+    public typealias Value = Value
+}
