c3c0b41f76f566ba08340e8038d400158d2eed91
Steffan Andrews <stef@orchetect.com>
2024-12-14 17:09:03 -0800

    Updated unit tests

---
+++ Tests/PrefsKitCoreTests/PrefsSchemaPrefsStorageTests.swift
@@ -0,0 +1,36 @@
+//
+//  PrefsSchemaPrefsStorageTests.swift
+//  PrefsKit • https://github.com/orchetect/PrefsKit
+//  © 2024 Steffan Andrews • Licensed under MIT License
+//
+
+import Foundation
+import PrefsKitCore
+import Testing
+
+@Suite
+struct PrefsSchemaStorageMacroTests {
+    @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+    @PrefsSchema final class TestSchemaA: @unchecked Sendable {
+        @Storage var storage = DictionaryPrefsStorage()
+        @StorageMode var storageMode = .cachedReadStorageWrite
+
+        @Pref var foo: Int?
+    }
+    
+    /// Note: public access level
+    @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+    @PrefsSchema public final class TestSchemaB: @unchecked Sendable {
+        @Storage public var storage = .dictionary
+        @StorageMode public var storageMode = .cachedReadStorageWrite
+        
+        // @Pref public var foo: Int? // TODO: fix this. need to be able to add access levels.
+    }
+    
+    /// No logic testing. Just ensure compiler is happy.
+    @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+    @Test func instantiate() {
+        let _ = TestSchemaA()
+        let _ = TestSchemaB()
+    }
+}
