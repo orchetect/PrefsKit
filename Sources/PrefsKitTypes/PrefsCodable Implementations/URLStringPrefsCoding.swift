539362e35e9937128d2903143ed73f96d46caa10
Steffan Andrews <stef@orchetect.com>
2024-12-31 13:04:00 -0800

    Added `URLStringPrefsCoding` coding strategy (#3)

---
+++ Sources/PrefsKitTypes/PrefsCodable Implementations/URLStringPrefsCoding.swift
@@ -0,0 +1,33 @@
+//
+//  URLStringPrefsCoding.swift
+//  PrefsKit • https://github.com/orchetect/PrefsKit
+//  © 2024 Steffan Andrews • Licensed under MIT License
+//
+
+import Foundation
+
+extension PrefsCodable where Self == URLStringPrefsCoding {
+    /// Coding strategy for `URL` using absolute `String` as the encoded storage value type.
+    ///
+    /// > Note:
+    /// >
+    /// > `URL` has native `Codable` conformance, which means it may also be used directly with
+    /// > `@JSONDataCodablePref` or `@JSONStringCodablePref`.
+    public static var urlString: URLStringPrefsCoding { .init() }
+}
+
+/// Coding strategy for `URL` using absolute `String` as the encoded storage value type.
+///
+/// > Note:
+/// >
+/// > `URL` has native `Codable` conformance, which means it may also be used directly with
+/// > `@JSONDataCodablePref` or `@JSONStringCodablePref`.
+public struct URLStringPrefsCoding: PrefsCodable {
+    public func encode(prefsValue: URL) -> String? {
+        prefsValue.absoluteString
+    }
+
+    public func decode(prefsValue: String) -> URL? {
+        URL(string: prefsValue)
+    }
+}
