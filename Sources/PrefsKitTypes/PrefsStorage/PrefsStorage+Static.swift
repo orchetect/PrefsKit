eaa340a174904d442d4e7d148590cce9e8c54ba6
Steffan Andrews <stef@orchetect.com>
2025-01-07 00:28:13 -0800

    `PrefsStorage`: Added `dictionary(unsafe:)` static constructor

--- Sources/PrefsKitTypes/PrefsStorage/PrefsStorage+Static.swift
+++ Sources/PrefsKitTypes/PrefsStorage/PrefsStorage+Static.swift
@@ -16,6 +16,12 @@ extension PrefsStorage where Self == DictionaryPrefsStorage {
     public static func dictionary(root: [String: any PrefsStorageValue]) -> DictionaryPrefsStorage {
         DictionaryPrefsStorage(root: root)
     }
+    
+    /// Dictionary prefs storage with initial raw untyped root content.
+    /// You are responsible for ensuring value types are compatible with related methods such as plist conversion.
+    public static func dictionary(unsafe storage: [String: Any]) -> DictionaryPrefsStorage {
+        DictionaryPrefsStorage(unsafe: storage)
+    }
 }
 
 extension PrefsStorage where Self == UserDefaultsPrefsStorage {
