3f185c52dca571472655c6823a92649d8a04c3a1
Steffan Andrews <stef@orchetect.com>
2025-08-29 13:38:55 -0700

    Updated unit tests

--- Tests/PrefsKitCoreTests/ActorTests.swift
+++ Tests/PrefsKitCoreTests/ActorTests.swift
@@ -8,18 +8,14 @@ import Foundation
 import PrefsKitCore
 import Testing
 
+/// Test `PrefsSchema` class with a `@Pref` member bound to `@MainActor`.
 @Suite
-struct ActorTests {
-    // Note:
-    //
-    // Can't attach @MainActor to the class because there are
-    // protocol requirements that can't be satisfied in that case
-    //
+struct MainActorMemberTests {
     @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
     @PrefsSchema final class TestSchema: @unchecked Sendable {
         @Storage var storage = .dictionary
         @StorageMode var storageMode = .cachedReadStorageWrite
-
+        
         @MainActor @Pref var foo: Int? // <-- can attach to individual properties
         @Pref var bar: String?
     }
@@ -31,5 +27,26 @@ struct ActorTests {
         
         Task { @MainActor in prefs.foo = 1 } // <-- needs MainActor context
         prefs.bar = "a string"
+    }
+}
+
+/// Test `PrefsSchema` class bound to `@MainActor`.
+@Suite
+struct MainActorBoundTests {
+    @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+    @MainActor @PrefsSchema public final class TestSchema {
+        @Storage var storage = .dictionary
+        @StorageMode var storageMode = .cachedReadStorageWrite
+        
+        @Pref var foo: Int?
+        @Pref var bar: String?
+    }
+    
+    @available(macOS 14.0, iOS 17.0, watchOS 10.0, tvOS 17.0, *)
+    @MainActor @Test
+    func mainActor() {
+        let prefs = TestSchema()
+        
+        prefs.foo = 1
     }
 }
